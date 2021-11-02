f42b84275d33c46aca55023a1a2baa28d2e977c4
Diana-nyamai <dnyamai.dn@gmail.com>
2021-11-02 16:12:43 +0300

    update

---
+++ LV/transversy/index.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            firstName: 'diana',
+            secondName: 'ndinda',
+            email: 'dnyamai.dn@gmail.com',
+            picture: ''
+        }
+    }
+})
+
+app.mount('#app')
