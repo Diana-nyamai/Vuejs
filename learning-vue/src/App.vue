8b9588604c51b10bebbbb23457675ebb7072e8da
Diana-nyamai <dnyamai.dn@gmail.com>
2021-11-04 15:58:22 +0300

    binding styles

--- learning-vue/src/App.vue
+++ learning-vue/src/App.vue
@@ -10,7 +10,8 @@
   <h3 :class="issoldout? 'soldout':'new'">sold out?</h3>
   <h3 :class="['new', 'promoted']">newly promoted movie</h3>
   <h3 :class="[ispromoted && 'promoted', issoldout? 'soldout': 'new']">aray of promoted movies</h3>
-
+  <h4 :class="{promoted: ispromoted, new: !issoldout, soldout: issoldout}">checking the movies</h4>
+<div :style="hightlight">highight styling</div>
    </div>
 </template>
 
@@ -23,8 +24,13 @@ export default {
       channel: '<b>codeevolution</b>',
       greeting: 'hello world',
       disabled: false,
-      ispromoted: false,
-      issoldout: false
+      ispromoted: true,
+      issoldout: true,
+      hightlight: {
+        fontSize: '50px',
+        color: 'yellow',
+        padding: '30px'
+      }
 
       }
       }
