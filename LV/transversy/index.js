ac95d36941265c606a61b5434796d649e9b467c5
Diana-nyamai <dnyamai.dn@gmail.com>
2021-11-04 10:09:02 +0300

    update

--- LV/transversy/index.js
+++ LV/transversy/index.js
@@ -1,11 +1,26 @@
 const app = Vue.createApp({
     data(){
         return{
-            firstName: 'diana',
-            secondName: 'ndinda',
+            firstName: 'jeremy',
+            secondName: 'kamama',
             email: 'dnyamai.dn@gmail.com',
+            gender: 'male',
             picture: 'https://randomuser.me/api/portraits/men/10.jpg',
             pic: './blue.jpg'
+        }
+    },
+    methods: {
+        async getUser(){
+          const res = await fetch('https://randomuser.me/api')
+          const {results} = await res.json()
+
+           console.log(results)
+
+            this.firstName = results[0].name.first
+            this.secondName = results[0].name.second
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
         }
     }
 })
