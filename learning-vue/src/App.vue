3e4e4ff50b2a775be0ae2f7ae1389150f451725e
Diana-nyamai <dnyamai.dn@gmail.com>
2021-11-05 17:42:57 +0300

    list rendering

--- learning-vue/src/App.vue
+++ learning-vue/src/App.vue
@@ -12,12 +12,19 @@
   <h3 :class="[ispromoted && 'promoted', issoldout? 'soldout': 'new']">aray of promoted movies</h3>
   <h4 :class="{promoted: ispromoted, new: !issoldout, soldout: issoldout}">checking the movies</h4>
 <div :style="hightlight">highight styling</div>
-<div><img :src="logo" alt="logo"></div> -->
+<div><img :src="logo" alt="logo"></div> introduction to vue and the basics -->
 
-<h2 v-if="num === 0">this number is zero</h2>
+<!-- <h2 v-if="num === 0">this number is zero</h2>
 <h2 v-else-if="num < 0">this number is negative</h2>
 <h2 v-else-if="num > 0">this number is positive</h2>
-<h2 v-else>this is not a number</h2>
+<h2 v-else>this is not a number</h2> the conditonal rendering-->
+<h1 v-for="(name,index) in names" :key="name">{{index}} {{name}}</h1>
+<h1 v-for="name in fullNames" :key="name.first">{{name.first}} {{name.second}}</h1>
+
+<div v-for="actor in actors" :key="actor.name">
+  <h3>{{actor.name}}</h3>
+  <h4 v-for="movie in actor.movies" :key="movie" :style="movieColor">{{movie}}</h4>
+</div>
    </div>
 </template>
 
@@ -26,7 +33,33 @@ export default {
   name: 'App',
   data(){
     return{
-     num: "diana"
+      names: ['diana', 'ndinda', 'nyamai'],
+      fullNames: [
+        {first: 'diana', second: 'nyamai'},
+         {first: 'princess', second: 'tiana'},
+          {first: 'queen', second: 'nyamai'}
+        
+      ],
+      actors: [
+        {
+          name: 'lee minho',
+          movies: [ 'the king', 'legend of blue sea']
+        },
+        {
+          name: 'lee jong suk',
+          movies: ['pinnochio', 'w: two world apart']
+        },
+        {
+          name: 'suzy',
+          movies: ['startp', 'while you were sleeping']
+        }
+      ],
+      movieColor: {
+        color: 'red'
+      }
+
+    //  num: "diana" (conditonal rendering data)
+    // {
       // name: 'ndinda',
       // channel: '<b>codeevolution</b>',
       // greeting: 'hello world',
@@ -39,7 +72,7 @@ export default {
       //   padding: '30px'
       // },
       // logo: 'https://randomuser.me/api/portraits/men/10.jpg'
-
+//  the basics of vue data}
       }
       }
 
@@ -55,6 +88,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.movies{
+  color: red;
+}
 /* .underline{
   text-decoration: underline;
 }
@@ -66,5 +102,5 @@ export default {
 }
 .soldout{
   color: red;
-} */
+} the basics of vue styling */
 </style>
